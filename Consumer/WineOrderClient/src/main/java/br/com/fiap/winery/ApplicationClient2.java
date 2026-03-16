6836090132b6a08856ad08b033b024bf26e190f4
Leandrns <leandro2005souza@gmail.com>
2026-03-16 10:13:27 -0300

    feat: consumindo web services

--- Consumer/WineOrderClient/src/main/java/br/com/fiap/winery/ApplicationClient2.java
+++ Consumer/WineOrderClient/src/main/java/br/com/fiap/winery/ApplicationClient2.java
@@ -1,4 +1,28 @@
 package br.com.fiap.winery;
 
+import javax.xml.namespace.QName;
+
+import br.com.fiap.winery.stubclasses.WineStockService;
+import jakarta.xml.ws.Service;
+import javax.xml.namespace.QName;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+
 public class ApplicationClient2 {
+    public static void main(String[] args) throws MalformedURLException {
+        final String wsdl = "http://localhost:8085/WineStockService?wsdl";
+        final String TARGET = "http://winery.fiap.com.br/";
+        final String NAME = "WineWarningServiceImplementationService";
+
+        URL url = new URL(wsdl);
+        QName qName = new QName(TARGET, NAME);
+        Service service = Service.create(url, qName);
+
+        WineStockService wineStockService = service.getPort(WineStockService.class);
+
+        String order = wineStockService.placeOrder("Dr. Loosen Riesling", 2);
+
+        System.out.println(order);
+    }
 }
