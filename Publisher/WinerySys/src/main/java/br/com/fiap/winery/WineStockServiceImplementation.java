7d874de4beead5f87e5817b1c3547696b41a6beb
Leandrns <leandro2005souza@gmail.com>
2026-03-16 10:25:56 -0300

    feat: formatando lista de vinhos

--- Publisher/WinerySys/src/main/java/br/com/fiap/winery/WineStockServiceImplementation.java
+++ Publisher/WinerySys/src/main/java/br/com/fiap/winery/WineStockServiceImplementation.java
@@ -7,78 +7,49 @@ public class WineStockServiceImplementation implements WineStockService {
     @Override
     public String getMenu() {
         return "Cabernet Sauvignon:\n" +
-                "\n" +
-                "Château Lafite Rothschild\n" +
-                "\n" +
-                "Caymus Cabernet Sauvignon\n" +
-                "\n" +
-                "Catena Zapata Cabernet Sauvignon\n" +
-                "\n" +
-                "Penfolds Bin 407 Cabernet Sauvignon\n" +
+                "- Château Lafite Rothschild\n" +
+                "- Caymus Cabernet Sauvignon\n" +
+                "- Catena Zapata Cabernet Sauvignon\n" +
+                "- Penfolds Bin 407 Cabernet Sauvignon\n" +
                 "\n" +
                 "Merlot:\n" +
-                "\n" +
-                "Pétrus\n" +
-                "\n" +
-                "Duckhorn Merlot\n" +
-                "\n" +
-                "Concha y Toro Marques de Casa Concha Merlot\n" +
-                "\n" +
-                "Stags' Leap Winery Merlot\n" +
+                "- Pétrus\n" +
+                "- Duckhorn Merlot\n" +
+                "- Concha y Toro Marques de Casa Concha Merlot\n" +
+                "- Stags' Leap Winery Merlot\n" +
                 "\n" +
                 "Pinot Noir:\n" +
-                "\n" +
-                "Domaine de la Romanée-Conti La Tâche\n" +
-                "\n" +
-                "Meiomi Pinot Noir\n" +
-                "\n" +
-                "Louis Jadot Pinot Noir\n" +
-                "\n" +
-                "Cloudline Pinot Noir\n" +
+                "- Domaine de la Romanée-Conti La Tâche\n" +
+                "- Meiomi Pinot Noir\n" +
+                "- Louis Jadot Pinot Noir\n" +
+                "- Cloudline Pinot Noir\n" +
                 "\n" +
                 "Malbec:\n" +
-                "\n" +
-                "Catena Zapata Malbec Argentino\n" +
-                "\n" +
-                "Trapiche Malbec\n" +
-                "\n" +
-                "Terrazas de los Andes Malbec\n" +
-                "\n" +
-                "Alamos Malbec\n" +
+                "- Catena Zapata Malbec Argentino\n" +
+                "- Trapiche Malbec\n" +
+                "- Terrazas de los Andes Malbec\n" +
+                "- Alamos Malbec\n" +
                 "\n" +
                 "Syrah (Shiraz):\n" +
-                "\n" +
-                "Penfolds Grange\n" +
-                "\n" +
-                "E. Guigal Côte-Rôtie\n" +
-                "\n" +
-                "Yellow Tail Shiraz\n" +
-                "\n" +
-                "Torbreck Woodcutter's Shiraz\n" +
+                "- Penfolds Grange\n" +
+                "- E. Guigal Côte-Rôtie\n" +
+                "- Yellow Tail Shiraz\n" +
+                "- Torbreck Woodcutter's Shiraz\n" +
                 "\n" +
                 "Chardonnay:\n" +
-                "\n" +
-                "Kendall-Jackson Vintner’s Reserve Chardonnay\n" +
-                "\n" +
-                "Louis Latour Corton-Charlemagne\n" +
-                "\n" +
-                "La Crema Sonoma Coast Chardonnay\n" +
+                "- Kendall-Jackson Vintner’s Reserve Chardonnay\n" +
+                "- Louis Latour Corton-Charlemagne\n" +
+                "- La Crema Sonoma Coast Chardonnay\n" +
                 "\n" +
                 "Sauvignon Blanc:\n" +
-                "\n" +
-                "Cloudy Bay Sauvignon Blanc\n" +
-                "\n" +
-                "Kim Crawford Sauvignon Blanc\n" +
-                "\n" +
-                "Oyster Bay Sauvignon Blanc\n" +
+                "- Cloudy Bay Sauvignon Blanc\n" +
+                "- Kim Crawford Sauvignon Blanc\n" +
+                "- Oyster Bay Sauvignon Blanc\n" +
                 "\n" +
                 "Riesling:\n" +
-                "\n" +
-                "Dr. Loosen Riesling\n" +
-                "\n" +
-                "Egon Müller Scharzhofberger Riesling\n" +
-                "\n" +
-                "Trimbach Riesling";
+                "- Dr. Loosen Riesling\n" +
+                "- Egon Müller Scharzhofberger Riesling\n" +
+                "- Trimbach Riesling";
     }
 
     @Override
